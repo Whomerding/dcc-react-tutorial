1fc74afb75a9658e0cdd0676c73b79a38e07efb8
Whomerding <wendy.homerding@gmail.com>
2023-03-09 12:45:45 -0500

    created chart tracker component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
-
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 function App() {
 
   const[entries, setEntries] = useState([{weight: 175, date: '11-25-2021'},{weight: 176, date: '11-26-2021'}])
@@ -15,6 +15,7 @@ function App() {
     <div>
       <DisplayEntries parentEntries={entries} /> 
       <AddEntryForm addNewEntry = {addNewEntry} />   
+      <EntriesChartTracker parentEntries = {entries}/>
     </div>
   );
 }
